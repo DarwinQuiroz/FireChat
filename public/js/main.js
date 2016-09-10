dc3d8d1ef2f50c2fc7940e155db82235ef37bf97
Darwin Quiroz <darwin_quiroz04@hotmail.com>
2016-09-10 11:46:36 -0500

    Visualizar usuarios conectados

--- public/js/main.js
+++ public/js/main.js
@@ -7,10 +7,19 @@ storageBucket: "firechat-caa66.appspot.com",
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+var userConect = null;
+var conectKey = "";
+var cont_user = 0;
+
 var userName = document.getElementById('user-name');
 var userImage = document.getElementById('user-pic');
+var userOnline = document.getElementById('cont-user');
+document.getElementById('btn-google').addEventListener('click', ingresoGoogle, false);
+document.getElementById('btn-facebook').addEventListener('click', ingresoFacebook, false);
+document.getElementById('cerrar').addEventListener('click', eliminarUserDB, false);
 
-
+// Iniciar Sesión con Google
 function ingresoGoogle()
 {
 	if(!firebase.auth().currentUser)
@@ -22,7 +31,8 @@ function ingresoGoogle()
 			var user = result.user;
 			console.log(user);
 			var name = result.user.displayName;
-			$('#page').css('display', 'none') && $('page2').css('display', 'block');
+			$('#page').css('display', 'none') && $('#page2').css('display', 'block');
+			inicializarFire();
 			// document.querySelector('span.mdl-layout-title').textContent = "Bienvenido " + name;
 		}).catch(function(error)
 		{
@@ -42,6 +52,7 @@ function ingresoGoogle()
 	}
 }
 
+// Iniciar Sesión con Facebook
 function ingresoFacebook()
 {
 	if(!firebase.auth().currentUser)
@@ -53,7 +64,8 @@ function ingresoFacebook()
 			var user = result.user;
 			// console.log(user);
 			var name = result.user.displayName;
-			$('#page').css('display', 'none') && $('page2').css('display', 'block');
+			$('#page').css('display', 'none') && $('#page2').css('display', 'block');
+			inicializarFire();
 			// document.querySelector('span.mdl-layout-title').textContent = "Bienvenido " + name;
 		}).catch(function(error)
 		{
@@ -82,15 +94,61 @@ function inicializarFire()
 				var displayName = user.displayName;
 				var userPhoto = user.photoURL;
 				userName.textContent = displayName;
-				userImage.style.backgroundImage = "url("+userPhoto+")";
+				if(userPhoto)
+				{
+					userImage.style.backgroundImage = "url("+userPhoto+")";
+				}
+				else 
+				{
+					userImage.style.backgroundImage = "url(../img/profile_placeholder.png)";
+				}
+				userConect = database.ref("/user");
+				agregarUserBD(user.uid,user.displayName);
+				userConect.on('child_added', function(data){
+					usuariosConectados(data.val().name, data.val().uid);
+					console.log("Ha ingresado a la sala "+data.val().name);
+				});
+				userConect.on('child_remove', function(data){
+					usuarioDesconectado(data.val().uid);
+					console.log(data.val().name+" ha cerrado sesión");
+				});
 			}
 		});
 }
 
-window.onload = function()
+function agregarUserBD(uid,name)
 {
-	inicializarFire();
+  var conectado = userConect.push({
+    uid:uid,
+    name:name
+  }); 
+  conectKey = conectado.key;
 }
 
-document.getElementById('btn-google').addEventListener('click', ingresoGoogle, false);
-document.getElementById('btn-facebook').addEventListener('click', ingresoFacebook, false);
+function eliminarUserDB()
+{
+	database.ref("/user/"+conectKey).remove();
+	$('#page2').css("display", "none") && $('#page').css("display", "block");
+}
+
+function usuariosConectados(name, uid)
+{
+	// var li = '<li id="'+uid+'" class="mdl-list__item"> <span class="mdl-list__item-primary-content>"'
+	var li = '<li id="'+uid+'" class="mdl-list__item"><span class="mdl-list__item-primary-content"><i class="material-icons mdl-list__item-icon">person</li>'
+			+name+'</span></li>';
+	$('#user-online').append(li);
+	cont_user += 1;
+	userOnline.textContent = cont_user;
+}
+
+// window.onload = function()
+// {
+// 	inicializarFire();
+// }
+
+function usuarioDesconectado(uid)
+{
+	$('#'+uid).remove();
+	cont_user -= 1;
+	userOnline.textContent = cont_user;
+}
